064f5eca0057d96de46bee0dab1b68787253cfdc
Tomo Hung <tomohung@gmail.com>
2017-08-14 23:55:08 +0800

    composition reducers

--- src/index.js
+++ src/index.js
@@ -4,24 +4,59 @@ import ReactDOM from 'react-dom';
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }]
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo;
-        } else {
-          return {
-            ...todo,
-            completed: !todo.completed
-          };
-        }
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
+};
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    deafult:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
+  };
 };
