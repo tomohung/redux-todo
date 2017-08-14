77f29a7fdf019c5d38f1a9b67ddca13371c188b7
Tomo Hung <tomohung@gmail.com>
2017-08-14 23:41:52 +0800

    add reducer for add todo and toggle todo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        } else {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+      });
+    default:
+      return state;
+  }
+};
