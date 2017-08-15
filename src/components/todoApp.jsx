fe9e7b1ad83a5eaeb5b6d42531b4a95ceeedd73c
Tomo Hung <tomohung@gmail.com>
2017-08-15 15:26:16 +0800

    refactor TodoApp into presentational components

--- src/components/todoApp.jsx
+++ src/components/todoApp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import { FilterLink } from './filterLink';
 
-export let nextTodoId = 0;
+let nextTodoId = 0;
 export class TodoApp extends Component {
   render() {
     const {
@@ -32,25 +32,14 @@ export class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-              onClick={() => {
-                  store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id: todo.id
-                  });
-              }}
-              style={{
-                textDecoration:
-              todo.completed ?
-              'line-through' :
-              'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          } />
         <p>
           Show:
           {' '}
@@ -83,7 +72,40 @@ export class TodoApp extends Component {
   };
 };
 
-export const getVisibleTodos = (
+// Presentational Components
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration:
+              completed ?
+              'line-through' :
+              'none'
+    }}>
+    {text}
+  </li>
+);
+
+const getVisibleTodos = (
   todos,
   filter
 ) => {
