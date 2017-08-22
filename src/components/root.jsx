4a58fed31fd80767cf5e867e06e19ec8d4a28975
Tomo Hung <tomohung@gmail.com>
2017-08-22 09:32:24 +0800

    using react-router@3.0.5 for compatible with tutorial
    since current version is updated to 4.1.2

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { TodoApp } from '../components/TodoApp';
 import { PropTypes } from 'prop-types';
+import { Router, Route, browserHistory } from 'react-router';
 
 const Root = ({ store }) => (
   <MuiThemeProvider>
     <Provider store={store}>
-      <TodoApp />
+      <Router history={browserHistory}>
+        <Route path="/" component={TodoApp} />
+      </Router>
     </Provider>
   </MuiThemeProvider>
 );
